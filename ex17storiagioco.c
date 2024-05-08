bcca114ae0bb42aa6d4b01c37245d267e7259d61
maddalena0543 <169042515+maddalena0543@users.noreply.github.com>
2024-05-08 18:11:25 +0000

    storia

--- ex17storiagioco.c
+++ ex17storiagioco.c
@@ -4,30 +4,58 @@
 int main()
 {
     char scelta;
+    char scelta1;
+    char scelta2;
     char nomeutente[50];
      printf("Benvenuto nel gioco!\n");
-     printf("Dovrai sceglere tra diverse opzioni per portare avanti la storia, in base alle tue scelta la storia cambierà!\n ");
+     printf("Dovrai sceglere tra diverse opzioni per portare avanti la storia, in base alle tue scelte la storia cambierà!\n ");
 
      printf("Inserisci il tuo Nome\n");
 
     scanf(" %s" ,nomeutente);
-    
-    printf(" Ciao %s iniziamo il racconto" , nomeutente); 
+    printf(" Ciao %s iniziamo il racconto, sei pronto?\n" , nomeutente); 
+    scanf(" %s" ,nomeutente);
+   
     printf(" inizio del racconto\n");
-    
     printf(" ora scegli tra l'Opzione A e l'Opzione B\n");
 
     scanf(" %c" , &scelta);
 
     if(scelta == 'A' || scelta == 'a')
     {
-        printf("hai scelto l'opzione A, continuo racconto\n");
-    
+        printf(" continuo racconto\n");
+        
+        scanf(" %c" , &scelta1);
+
+        if(scelta1 == 'A' || scelta1 == 'a')
+        {
+            printf("continuo testo\n");
+            printf("per creare una nuova storia scrivi RESTART\n ");
+        }
+        else if( scelta1 == 'B' || scelta1 == 'b')
+        {
+            printf(" continuo del testo2\n");
+            printf("per creare una nuova storia scrivi RESTART\n ");
+        }
     }
     else if( scelta == 'B' || scelta == 'b')
     {
-        printf("hai scelto l'opzione B, continuo testo\n");
+        printf("ontinuo testo\n");
+        
+        scanf(" %c" , &scelta2);
+
+        if(scelta2 == 'A' || scelta2 == 'a')
+        {
+            printf(" continuo testo1\n");
+            printf("per creare una nuova storia scrivi RESTART\n ");
+        }
+        else if(scelta2 == 'B' || scelta2 == 'b')
+        {
+            printf(" continuo testo2\n");
+            printf("per creare una nuova storia scrivi RESTART\n ");
+        }
     }
+    
     
 
 }
